598eb89973b3f568685def67e89139646ba0738f
Marcin <axlii50@gmail.com>
2020-12-23 11:47:27 +0100

    get scheduled photos route

---
+++ InstPect/Controllers/InstagramActions/GetScheduleController.ts
@@ -0,0 +1,25 @@
+import { getRepository } from 'typeorm';
+
+import InstagramPhoto from '../../Entities/InstagramPhoto';
+import { APIStatus } from '../../Middleware/Main/APIMiddleware';
+
+export default async function GetSchedule(req, res, next)
+{
+    try
+    {
+        let AccountID = req.body.accountid;
+
+        let PhotoRepository = getRepository(InstagramPhoto);
+        let Photos = await PhotoRepository.find({ where: { accountid: AccountID }, select: ["id", "photo", "date", "description"] });
+
+        return next(new APIStatus(200, Photos));
+    }
+    catch (Error)
+    {
+        switch (Error.code)
+        {
+            default:
+                return next(new APIStatus(500, Error));
+        }
+    }
+}
