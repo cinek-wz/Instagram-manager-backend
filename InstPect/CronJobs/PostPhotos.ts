f10f500995faef9cb649b66236fe09d2f9340252
Marcin <axlii50@gmail.com>
2020-12-23 08:06:46 +0100

    PostPhotos CRON Job

--- InstPect/CronJobs/PostPhotos.ts
+++ InstPect/CronJobs/PostPhotos.ts
@@ -1,4 +1,44 @@
+import { getRepository, LessThanOrEqual } from 'typeorm';
+
+import InstagramAccount from '../Entities/InstagramAccount';
+import InstagramPhoto from '../Entities/InstagramPhoto';
+
+import * as InstagramBaseModel from '../Models/InstagramBaseModel';
+
+import { IgApiClient } from "instagram-private-api";
+
 export default async function PostPhotos()
 {
+    try
+    {
+        let InstagramRepository = getRepository(InstagramAccount);
+        let PhotoRepository = getRepository(InstagramPhoto);
 
+        let CurrentTime: Date = new Date();
+
+        let Photos = await PhotoRepository.find({ relations: ["account"], where: { date: LessThanOrEqual(CurrentTime) } });
+
+        for (let Photo of Photos)
+        {
+            let Account: InstagramAccount = Photo.account;
+            let Client: IgApiClient = await InstagramBaseModel.GetInstagramAccountClient(Account);
+
+            let publishResult = await Client.publish.photo(
+            {
+                file: Photo.photo,
+                caption: Photo.description,
+            });
+
+            await PhotoRepository.delete(Photo);
+            if (publishResult.status != 'ok')
+            {
+                throw new Error(JSON.stringify(publishResult));
+            }
+        }
+    }
+    catch (Error)
+    {
+        console.error(`Error (CRON PostPhotos): ${Error}`);
+        return;
+    }
 }
