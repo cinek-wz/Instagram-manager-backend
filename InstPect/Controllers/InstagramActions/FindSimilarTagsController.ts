c0959254365b3a2cf510b3d337e7f148c8b38f6b
Marcin <axlii50@gmail.com>
2021-01-03 14:54:27 +0100

    tags fix

--- InstPect/Controllers/InstagramActions/FindSimilarTagsController.ts
+++ InstPect/Controllers/InstagramActions/FindSimilarTagsController.ts
@@ -1,10 +1,9 @@
-import * as InstagramModel from '../../Models/InstagramModel';
 import * as InstagramBaseModel from '../../Models/InstagramBaseModel';
-
 
 import { APIStatus } from '../../Middleware/Main/APIMiddleware';
 import ErrorEx from '../../Utils/Error';
 import InstagramAccount from '../../Entities/InstagramAccount';
+import { IgApiClient } from "instagram-private-api";
 
 export default async function FindSimilarTags(req, res, next) 
 {
@@ -13,10 +12,35 @@ export default async function FindSimilarTags(req, res, next)
         let Tag = req.body.tag;
         let InstagramAccount: InstagramAccount = req.instagramaccount;
 
-        let InstagramClient = await InstagramBaseModel.GetInstagramAccountClient(InstagramAccount);
-        let SimilarTags = await InstagramModel.GetSimilarTags(InstagramClient, Tag);
+        let InstagramClient: IgApiClient = await InstagramBaseModel.GetInstagramAccountClient(InstagramAccount);
 
-        return next(new APIStatus(200, SimilarTags));
+        let tags = await InstagramClient.feed.tags(Tag, "top");
+        await tags.request();
+        let Items = await tags.items();
+
+        // array
+        let Collection: Array<{ tag: string, count: number }> = [];
+
+        for (let Item of Items) {
+            if (Item.caption == null) continue;
+            let Match = Item.caption.text.match(/(#.*?)(?= |#|\n|$)/gm);
+            if (Match == null) continue;
+
+            for (let Tag of Match) {
+                let Existing = Collection.findIndex((element) => element.tag == Tag);
+
+                if (Existing == -1) {
+                    Collection.push({ tag: Tag, count: 1 });
+                }
+                else {
+                    Collection[Existing].count += 1;
+                }
+            }
+        }
+        Collection = Collection.sort((a, b) => (a.count - b.count < 0) ? 1 : -1);
+        Collection = Collection.slice(0, 30);
+
+        return next(new APIStatus(200, Collection));
     }
     catch (Error)
     {
